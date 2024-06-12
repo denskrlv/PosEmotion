74a5ed54fd95e85fd9ee792deb4bc88d8bed87bb
Denis Krylov <krylov.den@icloud.com>
2024-06-12 14:03:08 +0200

    Added grid search to SVM.

--- tools/structures.py
+++ tools/structures.py
@@ -84,8 +84,130 @@ class Segment:
 
         return emotion_count
     
+    def avg_torso_angle(self):
+        torso_angles = []
+        shoulder_coords = self.df[['left_shoulder_X', 'left_shoulder_Y', 'right_shoulder_X', 'right_shoulder_Y']].values
+        hip_coords = self.df[['left_hip_X', 'left_hip_Y', 'right_hip_X', 'right_hip_Y']].values
+
+        for i in range(len(shoulder_coords)):
+            shoulder_mid = ((shoulder_coords[i][0] + shoulder_coords[i][2]) / 2, (shoulder_coords[i][1] + shoulder_coords[i][3]) / 2)
+            hips_mid = ((hip_coords[i][0] + hip_coords[i][2]) / 2, (hip_coords[i][1] + hip_coords[i][3]) / 2)
+
+            dx = hips_mid[0] - shoulder_mid[0]
+            dy = hips_mid[1] - shoulder_mid[1]
+
+            rad = np.arctan2(dy, dx)
+            deg = np.rad2deg(rad)
+            torso_angles.append(deg)
+        
+        return np.mean(torso_angles)
     
+    def avg_head_position(self):
+        head_positions = []
+        nose_coords = self.df[['nose_X', 'nose_Y']].values
+        left_eye_coords = self.df[['left_eye_X', 'left_eye_Y']].values
+        right_eye_coords = self.df[['right_eye_X', 'right_eye_Y']].values
+        left_ear_coords = self.df[['left_ear_X', 'left_ear_Y']].values
+        right_ear_coords = self.df[['right_ear_X', 'right_ear_Y']].values
+
+        for i in range(len(nose_coords)):
+            head_position = [(nose_coords[i][0] + left_eye_coords[i][0] + right_eye_coords[i][0] + left_ear_coords[i][0] + right_ear_coords[i][0]) / 5,
+                             (nose_coords[i][1] + left_eye_coords[i][1] + right_eye_coords[i][1] + left_ear_coords[i][1] + right_ear_coords[i][1]) / 5]
+            if not np.isnan(head_position).any():
+                head_positions.append(head_position)
+
+        mean_head_positions = np.mean(head_positions, axis=0)
+
+        return mean_head_positions[0], mean_head_positions[1]
     
+    def avg_head_tilt(self):
+        head_tilts = []
+        left_eye_coords = self.df[['left_eye_X', 'left_eye_Y']].values
+        right_eye_coords = self.df[['right_eye_X', 'right_eye_Y']].values
+
+        for i in range(len(left_eye_coords)):
+            dx = right_eye_coords[i][0] - left_eye_coords[i][0]
+            dy = right_eye_coords[i][1] - left_eye_coords[i][1]
+
+            rad = np.arctan2(dy, dx)
+            deg = np.rad2deg(rad)
+            head_tilts.append(deg)
+        
+        return np.mean(head_tilts)
+    
+    def avg_velocity(self, pair, fps=24):
+        coodinates = self.df[pair].values
+        frame_numbers = self.df["Frame Number"].values
+
+        dist = np.linalg.norm(coodinates[1:] - coodinates[:-1], axis=1)
+
+        if frame_numbers[0] - frame_numbers[-1] == 0:
+            time = 1 / fps
+        else:
+            time = (frame_numbers[0] - frame_numbers[-1]) / fps
+
+        return abs(np.sum(dist) / time)
+    
+    def avg_acceleration(self, pair, fps=24):
+        coodinates = self.df[pair].values
+        frame_numbers = self.df["Frame Number"].values
+
+        dist = np.linalg.norm(coodinates[1:] - coodinates[:-1], axis=1)
+        if frame_numbers[0] - frame_numbers[-1] == 0:
+            time = 1 / fps
+        else:
+            time = (frame_numbers[0] - frame_numbers[-1]) / fps
+        velocity = abs(np.sum(dist) / time)
+
+        return velocity / time
+    
+    def avg_jerk(self, pair, fps=24):
+        coodinates = self.df[pair].values
+        frame_numbers = self.df["Frame Number"].values
+
+        dist = np.linalg.norm(coodinates[1:] - coodinates[:-1], axis=1)
+        if frame_numbers[0] - frame_numbers[-1] == 0:
+            time = 1 / fps
+        else:
+            time = (frame_numbers[0] - frame_numbers[-1]) / fps
+        velocity = abs(np.sum(dist) / time)
+        acceleration = velocity / time
+
+        return acceleration / time
+    
+    def avg_hands_position(self):
+        left_wrist_coords = self.df[['left_wrist_X', 'left_wrist_Y']].values
+        right_wrist_coords = self.df[['right_wrist_X', 'right_wrist_Y']].values
+
+        mean_left_wrist_coords = np.mean(left_wrist_coords, axis=0)
+        mean_right_wrist_coords = np.mean(right_wrist_coords, axis=0)
+
+        return (mean_left_wrist_coords[0], mean_left_wrist_coords[1]), (mean_right_wrist_coords[0], mean_right_wrist_coords[1])
+    
+    def avg_dist_to_body(self):
+        left_wrist_coords = self.df[['left_wrist_X', 'left_wrist_Y']].values
+        right_wrist_coords = self.df[['right_wrist_X', 'right_wrist_Y']].values
+        left_shoulder_coords = self.df[['left_shoulder_X', 'left_shoulder_Y']].values
+        right_shoulder_coords = self.df[['right_shoulder_X', 'right_shoulder_Y']].values
+        left_hip_coords = self.df[['left_hip_X', 'left_hip_Y']].values
+        right_hip_coords = self.df[['right_hip_X', 'right_hip_Y']].values
+
+        left_wrist_to_body = np.linalg.norm(left_wrist_coords - left_shoulder_coords, axis=1) + np.linalg.norm(left_wrist_coords - left_hip_coords, axis=1)
+        right_wrist_to_body = np.linalg.norm(right_wrist_coords - right_shoulder_coords, axis=1) + np.linalg.norm(right_wrist_coords - right_hip_coords, axis=1)
+
+        return np.mean(left_wrist_to_body), np.mean(right_wrist_to_body)
+    
+    def avg_posture_symmetry(self):
+        left_shoulder_coords = self.df[['left_shoulder_X', 'left_shoulder_Y']].values
+        right_shoulder_coords = self.df[['right_shoulder_X', 'right_shoulder_Y']].values
+        left_hip_coords = self.df[['left_hip_X', 'left_hip_Y']].values
+        right_hip_coords = self.df[['right_hip_X', 'right_hip_Y']].values
+
+        shoulder_symmetry = np.linalg.norm(left_shoulder_coords - right_shoulder_coords, axis=1)
+        hip_symmetry = np.linalg.norm(left_hip_coords - right_hip_coords, axis=1)
+
+        return np.mean(shoulder_symmetry), np.mean(hip_symmetry)
+
 
 class Skeleton:
 
